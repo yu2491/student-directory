6b9191457216b021b4d4d4b14de077ba3ed75c15
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-29 14:14:12 +0100

    exercise 1

--- directory_pre_exercises.rb
+++ directory_pre_exercises.rb
@@ -22,8 +22,8 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
