dfb010e586ca1bda5b3141b36f8d3bdbe584bc07
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-29 15:51:22 +0100

    exercise_4

---
+++ directory_exercise_4.rb
@@ -0,0 +1,42 @@
+# 4. Rewrite the each() method that prints all students using while or until control flow methods (Loops).
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(students)
+  index = 0
+  until index == students.length
+    puts "#{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    index += 1
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
