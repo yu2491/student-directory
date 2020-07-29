dfe339fecee5571d83c9dd782f9319749b4d2db0
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-29 15:42:34 +0100

    exercise 3

---
+++ directory_exercise_3.rb
@@ -0,0 +1,45 @@
+#Modify your program to only print the students whose name is shorter than 12 characters.
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def filter(students)
+  students.delete_if { |student| student[:name].length > 12 }
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great student(s) with a name shorter than 12 characters"
+end
+
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(filter(students))
+print_footer(students)
