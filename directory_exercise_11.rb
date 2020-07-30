0266e2d354dd43d362e83032744230593229b5a2
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-30 09:16:21 +0100

    exercise_11

---
+++ directory_exercise_11.rb
@@ -0,0 +1,44 @@
+#Find all typos in that file and correct them.
+# Use your experience, online documentation, etc. to find all mistakes.
+# Run the script in the terminal from time to time to make sure it works as it should.
+# Google the errors Ruby will give you, think about what they could mean, try different things but don't look the answer up
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+    end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
