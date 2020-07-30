f284d1e5892cde5afcf5fe94b36c227290c0a78d
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-30 09:21:19 +0100

    exercise_12

---
+++ directory_exercise_12.rb
@@ -0,0 +1,43 @@
+# What happens if the user doesn't enter any students? It will try to print an empty list.
+# How can you use an if statement (Control Flow) to only print the list if there is at least one student in there?
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+students = input_students
+if students.count >= 1
+  print_header
+  print(students)
+end
+print_footer(students)
