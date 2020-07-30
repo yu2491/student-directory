1017ff4fb65faca2f166e1e485cfa39577d8168e
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-30 17:29:31 +0100

    finished directory

--- directory.rb
+++ directory.rb
@@ -1,47 +1,4 @@
-@students = [] #empty array accessible  to all methods
-
-def save_students
-  #open the file for writing
-  file = File.open("students.csv", "w")
-  #iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]] #create array of student
-    csv_line = student_data.join(",") #convert to string
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = gets.chomp
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu # print the menu and ask the user what to do
-    process(gets.chomp) # read the input and do what the user has asked
-  end
-end
+@students = [] #empty array accessible to all methods
 
 def print_menu
   puts "1. Input the students"
@@ -51,10 +8,11 @@ def print_menu
   puts "9. Exit" #9 because we'll add more items
 end
 
-def show_students
-  print_header
-  print_student_list
-  print_footer
+def interactive_menu
+  loop do
+    print_menu # print the menu and ask the user what to do
+    process(STDIN.gets.chomp) # read the input and do what the user has asked
+  end
 end
 
 def process(selection)
@@ -74,6 +32,27 @@ def process(selection)
   end
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -89,4 +68,38 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]] #create array of student
+    csv_line = student_data.join(",") #convert to string
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? # get out of method if it isn't given
+  if File.exists?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist"
+    exit #quit the progam
+  end
+end
+
+try_load_students
 interactive_menu
