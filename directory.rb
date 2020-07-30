c1b8ccfce32d4a322d07f2fa17123d622143e08e
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-30 16:35:07 +0100

    load file method

--- directory.rb
+++ directory.rb
@@ -12,6 +12,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -38,6 +47,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" #9 because we'll add more items
 end
 
@@ -55,6 +65,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit # this will cause program to terminate
     else
