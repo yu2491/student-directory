c1713509ad86e3460f080abfb98eba3734f4e962
Yasmin Underdown <yunderdown1@gmail.com>
2020-07-31 16:32:41 +0100

    choose file method added

--- directory.rb
+++ directory.rb
@@ -1,24 +1,33 @@
 =begin
-How could you make the program load students.csv by default
-if no file is given on startup? Which methods would you need to change?
+The filename we use to save and load data (menu items 3 and 4) is hardcoded.
+Make the script more flexible by asking for the filename if the user chooses these menu items.
 =end
 
 @students = [] #empty array accessible to all methods
 
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to a file"
+  puts "4. Load a list from a file"
   puts "9. Exit" #9 because we'll add more items
 end
 
 def interactive_menu
   loop do
+    puts "What would you like to do? (select a number)"
     print_menu # print the menu and ask the user what to do
     process(STDIN.gets.chomp) # read the input and do what the user has asked
   end
+end
+
+def choose_a_file
+  puts "Please enter a filename or press enter for the default file 'students.csv':"
+  file = gets.strip
+  if file == ""
+    file = "students.csv"
+  end
+  file
 end
 
 def process(selection)
@@ -28,10 +37,11 @@ def process(selection)
     when "2"
       show_students
     when "3"
-      save_students
+      save_students(choose_a_file)
     when "4"
-      load_students
+      load_students(choose_a_file)
     when "9"
+      puts "Exiting program"
       exit # this will cause program to terminate
     else
       puts "I don't know what you meant, try again"
@@ -52,15 +62,12 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # get the first name
-  name = STDIN.gets.chomp
+  name = STDIN.gets.chomp # get the first name
   # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
     update_student_array(name,choose_cohort)
     puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = STDIN.gets.chomp
+    name = STDIN.gets.chomp # get another name from the user
   end
 end
 
@@ -86,36 +93,37 @@ def print_student_list
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students"
+  puts "Overall, we have 1 great student" if @students.count == 1
+  puts "Overall, we have #{@students.count} great students" if @students.count > 1
+  puts "Currently we have 0 students" if @students.count == 0
 end
 
-def save_students
-  #open the file for writing
-  file = File.open("students.csv", "w")
-  #iterate over the array of students
-  @students.each do |student|
+def save_students(filename)
+  file = File.open(filename, "w") #open the file for writing
+  @students.each do |student| #iterate over the array of students
     student_data = [student[:name], student[:cohort]] #create array of student
     csv_line = student_data.join(",") #convert to string
     file.puts csv_line
   end
   file.close
-  puts "File saved"
+  puts "Students saved to #{filename}"
 end
 
-def load_students(filename = "students.csv")
+def load_students(filename)
   file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     update_student_array(name, cohort)
   end
   file.close
-  puts "Loaded #{@students.count} from #{filename}"
+  puts "Students uploaded from #{filename}"
 end
 
 def try_load_students
   ARGV.first.nil? ? filename = "students.csv" : filename = ARGV.first #first argument from the command line
   if File.exists?(filename) #if it exists
     load_students(filename)
+    puts "Loaded #{@students.count} students from #{filename}"
   else # if it doesn't exist
     puts "Sorry, #{filename} doesn't exist"
     exit #quit the progam
